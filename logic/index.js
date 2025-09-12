875d657a0b0a71e61c39c980651755e06ad11bdb
Ian Vaserman <ivaserman@frba.utn.edu.ar>
2025-09-12 15:57:19 -0300

    new styles

--- logic/index.js
+++ logic/index.js
@@ -4,3 +4,11 @@ function goExample(){
 function goCreate(){
     window.location.href = "../templates/crear.html"
 }
+
+const cards = document.querySelectorAll('.card-inner');
+cards.forEach(card => {
+  card.addEventListener('click', () => {
+    card.classList.toggle('flipped');
+    
+  });
+});
